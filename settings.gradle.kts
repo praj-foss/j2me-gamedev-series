32deb0fc165bcf33a3d460ab031899d35d900a3c
Priyadarshi Raj <praj-foss@users.noreply.github.com>
2025-08-06 22:31:26 +0530

    fix: add java version checks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+plugins {
+    id("ch.essmann.gradle.check-java-version") version "1"
+}
+
+checkJavaVersion {
+    maximumJavaVersion.set(JavaLanguageVersion.of(8))
+}
+
 rootProject.name = "j2me-gamedev-series"
 
 include("example")
